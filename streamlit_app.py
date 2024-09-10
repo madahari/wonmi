1c20fc8e8aedac4c9307fd359b38e689bc027a78
sungil kang <68050246+madahari@users.noreply.github.com>
2024-09-11 00:03:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 from openai import OpenAI
 
 # OpenAI 클라이언트 초기화
-openai_api_key = st.secrets["openai"]["api_key"]
+API_KEY = st.secrets["openai"]["api_key"] #openai_api_key
 client = OpenAI(api_key  = openai_api_key)
 # NewsAPI의 API 키 입력
 # API_KEY = 'your_news_api_key'  # 발급받은 API 키로 교체
